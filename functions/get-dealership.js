8638598b5b7ac6b4bfd80f852890f465d4386f32
Mario Masoud <mario.masoud@gmail.com>
2023-08-21 22:40:32 +0300

    try ibm get-dealership solution

--- functions/get-dealership.js
+++ functions/get-dealership.js
@@ -9,8 +9,10 @@ function dbCloudantConnect() {
     return new Promise((resolve, reject) => {
         Cloudant(
             {
-                // eslint-disable-line
-                url: "https://1f8ba730-6274-498e-bdc8-06a9d13ea97d-bluemix.cloudantnosqldb.appdomain.cloud", //Enter your Cloudant service URL here
+                // eslint-disable-line'
+                iamApiKey: "0t4gHmiuNEaPBkDjsS39BnnfXXN_SJsq4ikGtRTz9Ma-",
+                url: "https://1f8ba730-6274-498e-bdc8-06a9d13ea97d-bluemix.cloudantnosqldb.appdomain.cloud",
+                // account: "1f8ba730-6274-498e-bdc8-06a9d13ea97d-bluemix",
             },
             (err, cloudant) => {
                 if (err) {
@@ -42,5 +44,47 @@ app.use(express.json());
 
 // Define a route to get all dealerships with optional state and ID filters
 app.get("/dealerships/get", (req, res) => {
-    const { state, id } = req.query;
+    const { state } = req.query;
+
+    if (state) {
+        // Fetch dealerships by state abbreviation
+        db.find(
+            {
+                selector: {
+                    st: state,
+                },
+            },
+            (err, result) => {
+                if (err) {
+                    console.error("Error fetching dealerships by state:", err);
+                    return res
+                        .status(500)
+                        .send("Something went wrong on the server.");
+                }
+
+                if (result.docs.length === 0) {
+                    return res.status(404).send("The state does not exist.");
+                }
+
+                return res.json(result.docs);
+            }
+        );
+    } else {
+        // Fetch all dealerships if no state provided
+        db.list({ include_docs: true }, (err, result) => {
+            if (err) {
+                console.error("Error fetching all dealerships:", err);
+                return res
+                    .status(500)
+                    .send("Something went wrong on the server.");
+            }
+
+            const dealerships = result.rows.map((row) => row.doc);
+            return res.json(dealerships);
+        });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
